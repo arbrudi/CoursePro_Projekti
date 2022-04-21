a4428a011000f8ee5a072ffd88bae1070aef419c
arbrudi <ar51896@ubt-uni.net>
2022-04-21 12:10:19 +0200

    Update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="indexstyle.css">
+    <link rel="stylesheet" type="text/css" href="css/indexstyle.css">
     <title>Welcome</title>
 </head>
 <body>
