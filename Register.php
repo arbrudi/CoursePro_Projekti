93c61b4d3b9ff0fa3344ec5c398ce5d5c59cdf7c
arbrudi <ar51896@ubt-uni.net>
2022-04-22 22:50:50 +0200

    login and register but no roles :/

--- Register.php
+++ Register.php
@@ -1,3 +1,29 @@
+<?php
+require 'function.php';
+
+if(!empty($_SESSION["id"])){
+  header("Location: index.php");
+}
+
+$register = new Register();
+
+if(isset($_POST["submit"])){
+  $result = $register->registration($_POST["name"],$_POST["surname"], $_POST["username"], $_POST["email"], $_POST["password"], $_POST["confirmpassword"]);
+
+  if($result == 1){
+    echo
+    "<script> alert('Registration Successful'); </script>";
+  }
+  elseif($result == 10){
+    echo
+    "<script> alert('Username or Email Has Already Taken'); </script>";
+  }
+  elseif($result == 100){
+    echo
+    "<script> alert('Password Does Not Match'); </script>";
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,9 +54,12 @@
             <input type="password" name="password" placeholder="Password" required>
         </div>
         <div class="input-group">
-           <button name="submit" class="bttn">Register</button>
+            <input type="password" name="confirmpassword" placeholder="Confirm your password" required>
         </div>
-        <p class="login-register-text">You already have an account? <a href="#">Login Here.</a></p>
+        <div class="input-group">
+           <button type="submit" name="submit" class="bttn">Register</button>
+        </div>
+        <p class="login-register-text">You already have an account? <a href="login.php">Login Here.</a></p>
         </form>
     </div>
 
--- index.php
+++ index.php
@@ -1,55 +1,23 @@
+<?php
+require 'function.php';
 
+$select = new Select();
+
+if(!empty($_SESSION["id"])){
+  $user = $select->selectUserById($_SESSION["id"]);
+}
+else{
+  header("Location: login.php");
+}
+?>
 <!DOCTYPE html>
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/indexstyle.css">
-    <title>Welcome</title>
-</head>
-<body>
-<header>
-    <a href="#" class="brand">CoursePro</a>
-     <div class="menu-btn"></div>
-
-    <div class="navigation">
-        <div class="navigation-items">
-            <a href="#">Home</a>
-            <a href="#">Services</a>
-            <a href="#">About Us</a>
-            <a href="#">Contact Us</a>
-           
-     </div> 
-    </div>
-</header>
-
-<div class="w" id="w">
-    <p>Welcome to CoursePro!</p>
-    <hr>
-    <br>
-  <h4>In a world where the internet has taken over, CoursePro is the key to your education.
-  You can find plenty of educational materials right here.</h4>
-
-<h4>Knowledge is power, and it's right in front of you. Take it and wield it!</h4>
-<br>
-<h4>Do you want to become a part of our growing family?</h4>
-<div class="here">
-<a href="login.php">Click here!</a>
-</div>
-</div> 
-<script type="text/javascript">
-       //pjesa e javascript per nav menu
-       const menuBtn = document.querySelector('.menu-btn');
-        const navigation = document.querySelector(".navigation");
-       menuBtn.addEventListener("click", () => {
-           menuBtn.classList.toggle("active");
-           navigation.classList.toggle("active");
-
-       });
-   </script>
-</body>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Index</title>
+  </head>
+  <body>
+    <h1>Welcome <?php echo $user["name"]; ?></h1>
+    <a href="logout.php">Logout</a>
+  </body>
 </html>
-
